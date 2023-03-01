c7369f92bd80d87132643a44516fa19a51347ebc
shee-la <sheilayvonne45@gmail.com>
2023-03-01 16:16:23 +0300

    commit of the changed file name and edited files

--- anotherpage.php
+++ anotherpage.php
@@ -7,6 +7,6 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>LOGIN DONE</h1>
+    <h1>Another page</h1>
 </body>
 </html>
--- loginlogic.php
+++ loginlogic.php
@@ -8,7 +8,7 @@ $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 $count = mysqli_num_rows($result);
 if($count==1){
-    header("Location:welcome.php");
+    header("Location:anotherpage.php");
 }
 else{
     echo '<script>
